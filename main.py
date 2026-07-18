5640a58af7f28e9fbf0adcd858bc2745c5f6631e
samuel-ishimwe203 <samuelishimw02@gmail.com>
2026-07-18 12:06:43 +0200

    feat: python simple game

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # how to see output in python
 print ('hello samuel')
 print('welcome to tumba')
-# how to recieve input 
+#how to recieve input 
 
 name= input('what is your name? ')
 color= input('what is your favorite color? ')
 print(name + " likes "+ color)
 
-# 
+birth_day= input('what is your birth day: ')
+age= 2026 - int(birth_day)
+print(age) 
+
---
+++ simpleGame.py
@@ -0,0 +1,18 @@
+#simple game
+
+
+secret_number= 9
+guess_count=0
+limited_guess=3
+
+while guess_count<limited_guess:
+    guess= int(input('guess: '))
+    guess_count+=1
+    if guess==secret_number:
+        print('you won')
+        break
+else:
+    print('please you loss')    
+
+
+
