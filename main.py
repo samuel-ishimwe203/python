a04e3bbc84517c03a51f8556cc1b83150a784dd3
samuel-ishimwe203 <samuelishimw02@gmail.com>
2026-07-18 04:21:47 +0200

    feat: first project in python

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+# how to see output in python
 print ('hello samuel')
 print('welcome to tumba')
+# how to recieve input 
+
+name= input('what is your name? ')
+color= input('what is your favorite color? ')
+print(name + " likes "+ color)
+
+# 
