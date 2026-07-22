f464fde879cfc6aa29cc415425f00f5eab071a1e
samuel-ishimwe203 <samuelishimw02@gmail.com>
2026-07-22 12:08:54 +0200

    feat: remove duplication in list of number

--- findLagestNumber.py
+++ findLagestNumber.py
@@ -14,3 +14,22 @@ numbers= [2,3,5,6,8,9,10]
 for number in numbers:
     if number % 2 != 0: 
       print(number)
+
+# add number to the list
+
+list= [10,3,4,89,3,3,0,2]
+
+list.append(30)
+list.insert(2, 100)
+list.remove(3)
+print(list)
+
+# remove duplication in list
+
+list2= [2,3,4,1,2,3,4,5,3,4,5,7,8,6,8,5,7]
+unique= []
+for number in list2:
+    if number not in unique:
+        unique.append(number)
+        unique.sort()
+print(unique)
